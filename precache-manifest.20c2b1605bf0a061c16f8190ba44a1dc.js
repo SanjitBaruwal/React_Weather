0f9f55d53302b44f065fcd162ac0ea346ee895da
Sanjit Baruwal <baruwalsanjit@gmail.com>
2023-06-06 01:52:42 +0545

    Updates

---
+++ precache-manifest.20c2b1605bf0a061c16f8190ba44a1dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19bd86af466630c92794",
+    "url": "/React_Weather/static/css/main.3f03865c.chunk.css"
+  },
+  {
+    "revision": "19bd86af466630c92794",
+    "url": "/React_Weather/static/js/main.19bd86af.chunk.js"
+  },
+  {
+    "revision": "55406fcb43a901795cc8",
+    "url": "/React_Weather/static/js/1.55406fcb.chunk.js"
+  },
+  {
+    "revision": "fe2a697cdd7173d48b1f",
+    "url": "/React_Weather/static/js/runtime~main.fe2a697c.js"
+  },
+  {
+    "revision": "4ce1d5556a6a86204a84e36c5f803814",
+    "url": "/React_Weather/index.html"
+  }
+];
